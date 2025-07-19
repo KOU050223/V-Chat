3f626e8ea20489c6c01c1365ce541df78db3aa5a
KOU050223 <kouhei20050223@icloud.com>
2025-07-20 06:20:19 +0900

    chore: update Radix UI dependencies to latest versions

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vroid-hub.pximg.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;

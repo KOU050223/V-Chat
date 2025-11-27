1e346c823f0a7e17dd9dd3f63d6572d75d95a7c1
KOU050223 <kouhei20050223@icloud.com>
2025-11-27 20:52:29 +0900

    chore(config): prettierをバージョン3.7.1に追加し設定を更新

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -1,70 +1,70 @@
-const { defineConfig, globalIgnores } = require('eslint/config');
+const { defineConfig, globalIgnores } = require("eslint/config");
+const globals = require("globals");
+const { fixupConfigRules, fixupPluginRules } = require("@eslint/compat");
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const _import = require("eslint-plugin-import");
+const js = require("@eslint/js");
+const { FlatCompat } = require("@eslint/eslintrc");
+const path = require("node:path");
 
-const globals = require('globals');
-
-const { fixupConfigRules, fixupPluginRules } = require('@eslint/compat');
-
-const tsParser = require('@typescript-eslint/parser');
-const typescriptEslint = require('@typescript-eslint/eslint-plugin');
-const _import = require('eslint-plugin-import');
-const js = require('@eslint/js');
-
-const { FlatCompat } = require('@eslint/eslintrc');
-
-const path = require('node:path');
+const prettierConfig = require("eslint-config-prettier");
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 module.exports = defineConfig([
-    {
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
 
-            parser: tsParser,
-            sourceType: 'module',
+      parser: tsParser,
+      sourceType: "module",
 
-            parserOptions: {
-                project: [
-                    path.join(__dirname, 'tsconfig.json'),
-                    path.join(__dirname, 'tsconfig.dev.json'),
-                ],
-                tsconfigRootDir: __dirname,
-            },
-        },
-
-        extends: fixupConfigRules(
-            compat.config({
-                extends: [
-                    'eslint:recommended',
-                    'plugin:import/errors',
-                    'plugin:import/warnings',
-                    'plugin:import/typescript',
-                    'google',
-                    'plugin:@typescript-eslint/recommended',
-                ],
-            })
-        ),
-
-        plugins: {
-            '@typescript-eslint': fixupPluginRules(typescriptEslint),
-            import: fixupPluginRules(_import),
-        },
-
-        rules: {
-            quotes: ['error', 'double'],
-            'import/no-unresolved': 0,
-            indent: ['error', 2],
-            'max-len': 'off',
-            // ESLint 9.x で削除されたルールを無効化
-            'valid-jsdoc': 'off',
-            'require-jsdoc': 'off',
-        },
+      parserOptions: {
+        project: [
+          path.join(__dirname, "tsconfig.json"),
+          path.join(__dirname, "tsconfig.dev.json"),
+        ],
+        tsconfigRootDir: __dirname,
+      },
     },
-    globalIgnores(['lib/**/*', 'generated/**/*', 'eslint.config.js']),
+
+    // 既存の extends 設定
+    extends: fixupConfigRules(
+      compat.config({
+        extends: [
+          "eslint:recommended",
+          "plugin:import/errors",
+          "plugin:import/warnings",
+          "plugin:import/typescript",
+          "google",
+          "plugin:@typescript-eslint/recommended",
+        ],
+      })
+    ),
+
+    plugins: {
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      import: fixupPluginRules(_import),
+    },
+
+    rules: {
+      "import/no-unresolved": 0,
+      "max-len": "off",
+      "valid-jsdoc": "off",
+      "require-jsdoc": "off", // 既存の設定を維持
+    },
+  },
+
+  // --- Prettier との連携 ---
+  // 🚨 最も最後に配置し、他の設定を上書きして競合を無効化します
+  prettierConfig,
+
+  globalIgnores(["lib/**/*", "generated/**/*", "eslint.config.js"]),
 ]);
